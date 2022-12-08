2fe20bd6be716d268c70ab51bbb5c69e1fb8c1eb
Syed Ishtiaque Ahmad <syed@dr-bill.ca>
2022-12-07 22:41:00 -0800

    add rendering and multithreading logic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use std::error::Error;
-use std::io;
+use std::{io, thread};
+use std::sync::mpsc;
 use std::time::Duration;
 use crossterm::{event, ExecutableCommand, terminal};
 use crossterm::cursor::{Hide, Show};
 use crossterm::event::{Event, KeyCode};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use rusty_audio::Audio;
+use space_invaders::{frame, render};
+use space_invaders::frame::new_frame;
 
 fn main() -> Result<(), Box<dyn Error>>{
     let mut audio = Audio::new();
@@ -21,11 +24,34 @@ fn main() -> Result<(), Box<dyn Error>>{
     let mut stdout  = io::stdout();
     terminal::enable_raw_mode()?;
     stdout.execute(EnterAlternateScreen)?;
-    stdout.execute(Hide);
+    stdout.execute(Hide)?;
+
+    // Render loop in a separate thread
+
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn( move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
 
     // Game loop
 
     'gameloop: loop {
+        // per frame init
+        let curr_frame = new_frame();
+
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -38,9 +64,15 @@ fn main() -> Result<(), Box<dyn Error>>{
                 }
             }
         }
+
+        // Draw & render
+        let _ = render_rx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     // cleanup
+    drop(render_tx);
+
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
